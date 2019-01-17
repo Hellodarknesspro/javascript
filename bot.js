3f76e370c17fc7846a8ab536ee58a53bce04f7ba
javascriptzzz <46732539+javascriptzzz@users.noreply.github.com>
2019-01-17 09:45:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "d";
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'hmm') {
+        message.react('🤔');
+    }
+});
+
+client.on('message', message => {
+    if (message.content === '-avatar') {
+        message.reply(message.author.displayAvatarURL);
+    }
+});
+
+client.on('message', message => {
+    if (message.content === '-ping') {
+        message.channel.send({embed:{
+            color: 0xd1250e,
+            description: "Pong  " + Math.round(client.ping) + "ms " + "🏓"
+     }
+ 
+client.login(process.env.BOT_TOKEN);
